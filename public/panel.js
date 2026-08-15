d11dd3f1406a8b2c23a0efc4f6b09b4a0b1496d4
3rr0r404w4sn0tf0und-sys <3rr0r404w4sn0tf0und@gmail.com>
2026-08-14 21:50:26 -0400

    Add files via upload

--- public/panel.js
+++ public/panel.js
@@ -1,3 +1,13 @@
+/**
+ * Onshape -> GitHub side-panel app frontend.
+ *
+ * Copyright (C) 2026  WanChengJunWang
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version. See the LICENSE file for the full text.
+ */
+
 const params = new URLSearchParams(window.location.search);
 const ctx = {
   documentId: params.get("documentId"),
